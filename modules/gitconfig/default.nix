ddc2671b34b01754e4d5172e846c8c0edf915a57
Emanuel Ramirez <eramirez2718@gmail.com>
2024-01-31 21:46:07 -0400

    git: set nvim as git editor

--- modules/gitconfig/default.nix
+++ modules/gitconfig/default.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  rg = "${pkgs.ripgrep}/bin/rg";
+in {
  programs.git = {
     enable = true;
     userName = "Emanuel Ramirez";
@@ -7,9 +10,16 @@
       color.ui = true;
       github.user = "emanuel2718";
       init.defaultBranch = "master";
+      pull.rebase = true;
+      core = {
+        editor = "nvim";
+        pager = "diff-so-fancy | less --tabs=4 -RFX";
+      };
     };
     aliases = {
       prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+      fixup = "!f(){ git reset --soft HEAD~\${1} && git commit --amend -C HEAD; };f";
+      loc = "!f(){ git ls-files | ${rg} \"\\.\${1}\" | xargs wc -l; };f"; # lines of code
     };
     #signing = {
     #  key = "";
