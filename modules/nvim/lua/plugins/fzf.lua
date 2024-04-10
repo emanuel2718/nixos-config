ec498ef8fe08c9bc60f02816f54aa9c82bcfe256
emanuel2718 <eramirez2718@gmail.com>
2024-04-10 18:44:15 -0400

    remove old structure

--- modules/nvim/lua/plugins/fzf.lua
+++
@@ -1,6 +0,0 @@
-local fzf = require('fzf-lua')
-fzf.setup {}
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
-map('n', '<leader>ff', function() fzf.files { previewer = false, hidden = true } end, opts)
