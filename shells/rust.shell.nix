e266f49915135d1ac6c348cf000a596f4f2589e8
Emanuel Ramirez <eramirez2718@gmail.com>
2024-07-28 09:56:55 -0400

    add .cargo/bin to the path on the rust nix shell

--- shells/rust.shell.nix
+++ shells/rust.shell.nix
@@ -1,5 +1,6 @@
-{ pkgs ? import (fetchTarball
-  "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { } }:
+{
+  pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { },
+}:
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     rustc
@@ -10,6 +11,12 @@ pkgs.mkShell {
     rust-analyzer
   ];
 
+  # Some crates require .cargo/bin to be on the path
+  shellHook = ''
+    export PATH=$PATH:$HOME/.cargo/bin
+  '';
+
   # Certain Rust tools won't work without this
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 }
+
