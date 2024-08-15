3d2aa16b099ea7b92d6f7ecfe76e763971e9a74a
emanuel2718 <eramirez2718@gmail.com>
2024-08-14 21:03:18 -0400

    remove old code

--- modules/fish/fish_user_key_bindings.fish
+++
@@ -1,2 +0,0 @@
-bind -M insert \cf accept-autosuggestion
-bind \cf "tmux-sessionizer"
--- modules/nvim/lua/plugins/term.lua
+++
@@ -1,5 +0,0 @@
-require('toggleterm').setup {
-  direction = 'float'
-}
-vim.keymap.set("n", "<C-t>", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true })
-vim.keymap.set("t", "<C-t>", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true })
