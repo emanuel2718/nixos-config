e4ecd7cf22d2aa5accbe86fa489ce85db00771cc
Emanuel Ramirez <eramirez2718@gmail.com>
2026-02-22 00:01:15 -0400

    add ctrl-f for fish auto completion

--- users/rami/config.fish
+++ users/rami/config.fish
@@ -5,6 +5,10 @@ function fish_mode_prompt; end
 
 set -gx EDITOR nvim
 
+bind -M insert \cf accept-autosuggestion
+bind -M default \cf accept-autosuggestion
+bind -M visual \cf accept-autosuggestion
+
 
 abbr -a tk 'tmux kill-session -t'
 abbr -a ta 'tmux a -t'
